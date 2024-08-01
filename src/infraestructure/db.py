2fcefd2591df34807b370634c5a385db5d92b539
DiegoAP5 <213475@ids.upchiapas.edu.mx>
2024-08-01 00:35:07 -0600

    update

--- src/infraestructure/db.py
+++ src/infraestructure/db.py
@@ -3,12 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Obtener datos de conexión desde variables de entorno
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-
 DATABASE_URL = f"mysql+pymysql://admin:alberto05@bazar.c36mijirazht.us-east-1.rds.amazonaws.com/seller"
 
 engine = create_engine(DATABASE_URL)
